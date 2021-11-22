3feba58e0d29fc069da16cb4c323e0354986f85e
teaspoon016 <praiseeze906@gmail.com>
2021-11-22 11:04:50 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,444 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- Basic -->
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<!-- Mobile Metas -->
+	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+	<!-- Site Metas -->
+	<title>Our wedding #Blessing & #Kelechi</title>
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<!-- Site Icons -->
+	<link rel="shortcut icon" href="images/ourwedding-1.png" type="image/x-icon">
+	<link rel="apple-touch-icon" href="images/ourwedding-1.png">
+	<script src="https://kit.fontawesome.com/e8189897cf.js" crossorigin="anonymous"></script>
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<!-- Pogo Slider CSS -->
+	<link rel="stylesheet" href="css/pogo-slider.min.css">
+	<!-- Site CSS -->
+	<link rel="stylesheet" href="css/style.css">
+	<!-- Responsive CSS -->
+	<link rel="stylesheet" href="css/responsive.css">
+	<!-- Custom CSS -->
+	<link rel="stylesheet" href="css/custom.css">
+
+</head>
+
+<body id="home" data-spy="scroll" data-target="#navbar-wd" data-offset="98">
+
+	<!-- LOADER -->
+	<div id="preloader">
+		<div class="preloader pulse">
+			<i class="fa fa-heartbeat" aria-hidden="true"></i>
+		</div>
+	</div><!-- end loader -->
+	<!-- END LOADER -->
+
+	<!-- Start header -->
+	<header class="top-header">
+		<nav class="navbar header-nav navbar-expand-lg">
+			<div class="container">
+				<a class="navbar-brand" href="index.html"><img src="images/ourwedding-2.png" style="width: 120px;" alt="image"></a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-wd" aria-controls="navbar-wd" aria-expanded="false" aria-label="Toggle navigation">
+					<span></span>
+					<span></span>
+					<span></span>
+				</button>
+				<div class="collapse navbar-collapse justify-content-end" id="navbar-wd">
+					<ul class="navbar-nav">
+						<li><a class="nav-link active" href="#home">Home</a></li>
+						<li><a class="nav-link" href="#about">About Us</a></li>
+						<li><a class="nav-link" href="#story">Our Story</a></li>
+						<li><a class="nav-link" href="#family">Family</a></li>
+						<li><a class="nav-link" href="#gallery">Gallery</a></li>
+						<li><a class="nav-link" href="#events">Events</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</header>
+	<!-- End header -->
+
+	<!-- Start Banner -->
+	<div class="ulockd-home-slider">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="pogoSlider" id="js-main-slider">
+					<div class="pogoSlider-slide" data-transition="zipReveal" data-duration="1500" style="background-image:url(images/WhatsApp\ Image\ 2021-11-18\ at\ 7.10.05\ PM.jpeg);">
+						<div class="lbox-caption">
+							<div class="lbox-details">
+								<h1>#Blessing & #Kelechi</h1>
+								<h2>We're getting married</h3>
+									<p>Save The Date <strong>18 December 2021</strong></p>
+							</div>
+						</div>
+					</div>
+					<div class="pogoSlider-slide" data-transition="blocksReveal" data-duration="1500" style="background-image:url(images/WhatsApp\ Image\ 2021-11-18\ at\ 7.10.05\ PM\ \(1\).jpeg);">
+						<div class="lbox-caption">
+							<div class="lbox-details">
+								<h1>#Blessing & #Kelechi</h1>
+								<h2>We're getting married</h3>
+									<p>Save The Date <strong>18 December 2021</strong></p>
+							</div>
+						</div>
+					</div>
+					<div class="pogoSlider-slide" data-transition="shrinkReveal" data-duration="2000" style="background-image:url(images/WhatsApp\ Image\ 2021-11-18\ at\ 7.10.06\ PM.jpeg);">
+						<div class="lbox-caption">
+							<div class="lbox-details">
+								<h1>#Blessing & #Kelechi</h1>
+								<h2>We're getting married</h3>
+									<p>Save The Date <strong>18 December 2021</strong></p>
+							</div>
+						</div>
+
+					</div>
+				</div><!-- .pogoSlider -->
+			</div>
+		</div>
+	</div>
+	<!-- End Banner -->
+
+	<!-- Start About us -->
+	<div id="about" class="about-box">
+		<div class="about-a1">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="title-box">
+							<h2>Blessing <span>&</span> Kelechi</h2>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-12 col-md-12 col-sm-12">
+						<div class="row align-items-center about-main-info">
+							<div class="col-lg-8 col-md-6 col-sm-12">
+								<h2> About <span>Blessing</span></h2>
+								<p>My name is Blessing Oforitselaju Tedeye, famously known as Blessing or Blessolala. A graduate of Journalism from the prestigious Jayee University College, Accra, I also attended NTA college Jos for a period of 2years and obtained ND in Journalism I'm a happy baby girl full of life, I love God and everything concerning Him. I love to cook for Africa & a business entrepreneur. </p>
+							</div>
+							<div class="col-lg-4 col-md-6 col-sm-12">
+								<div class="about-img">
+									<img class="img-fluid rounded" src="images/blessing.jpeg" alt="" />
+								</div>
+							</div>
+						</div>
+						<div class="row align-items-center about-main-info">
+							<div class="col-lg-4 col-md-6 col-sm-12">
+								<div class="about-img">
+									<img class="img-fluid rounded" src="images/kelechi.jpeg" alt="" />
+								</div>
+							</div>
+							<div class="col-lg-8 col-md-6 col-sm-12">
+								<h2> About <span>Kelechi</span></h2>
+								<p>My name is Kelechi Njoku, a goal getter, hard working, peaceful and a kind hearted guy that always want everyone around me to be happy at all times, above all I love doing the things of God. </p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End About us -->
+
+	<!-- Start Story -->
+	<div id="story" class="story-box main-timeline-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="title-box">
+						<h2>Our Story</h2>
+						<p>The Beginning. </p>
+					</div>
+				</div>
+			</div>
+			<div class="row timeline-element separline">
+				<div class="timeline-date-panel col-xs-12 col-md-6  align-left">
+					<div class="time-line-date-content">
+						<p class="mbr-timeline-date mbr-fonts-style display-font">
+							Sometime in 2020
+						</p>
+					</div>
+				</div>
+				<span class="iconBackground"></span>
+				<div class="col-xs-12 col-md-6 align-left">
+					<div class="timeline-text-content">
+						<h4 class="mbr-timeline-title pb-3 mbr-fonts-style display-font">First meet</h4>
+						<p class="mbr-timeline-text mbr-fonts-style display-7">
+							We met at a friends birthday party, and immediately found a common language, so some months later, our first date happened.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="row timeline-element reverse separline">
+				<div class="timeline-date-panel col-xs-12 col-md-6  align-left">
+					<div class="time-line-date-content">
+						<p class="mbr-timeline-date mbr-fonts-style display-font">
+							Fastforward to post lockdown
+						</p>
+					</div>
+				</div>
+				<span class="iconBackground"></span>
+				<div class="col-xs-12 col-md-6 align-right">
+					<div class="timeline-text-content">
+						<h4 class="mbr-timeline-title pb-3 mbr-fonts-style display-font">First date</h4>
+						<p class="mbr-timeline-text mbr-fonts-style display-7">
+							We met again, got talking again. The he confessed his love to me and that was the begining of our love story.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="row timeline-element separline">
+				<div class="timeline-date-panel col-xs-12 col-md-6  align-left">
+					<div class="time-line-date-content">
+						<p class="mbr-timeline-date mbr-fonts-style display-font">
+							8 April 2021
+						</p>
+					</div>
+				</div>
+				<span class="iconBackground"></span>
+				<div class="col-xs-12 col-md-6 align-left">
+					<div class="timeline-text-content">
+						<h4 class="mbr-timeline-title pb-3 mbr-fonts-style display-font">Proposal</h4>
+						<p class="mbr-timeline-text mbr-fonts-style display-7">
+							At about 20:53hrs Nigerian time, at a pool garden to be exact, I proposed, and then her words were "Yes, I will marry you”. With the greatest joy ever, I put an engagement ring on that charming finger.
+						</p>
+					</div>
+				</div>
+			</div>
+
+
+		</div>
+	</div>
+	<!-- End Story -->
+
+	<!-- Start Family -->
+	<div id="family" class="family-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="title-box">
+						<h2>Family</h2>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-4 col-md-6 col-sm-12">
+					<div class="single-team-member">
+						<div class="family-img">
+							<img class="img-fluid" src="images/fob.png" alt="" />
+						</div>
+						<div class="family-info">
+							<h4>Pastor Aniebiet Abia </h4>
+							<p>{ Brides Father }</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6 col-sm-12">
+					<div class="single-team-member">
+						<div class="family-img">
+							<img class="img-fluid" src="images/mob.png" alt="" />
+						</div>
+						<div class="family-info">
+							<h4>Mrs Florence Abia </h4>
+							<p>{ Brides Mother }</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6 col-sm-12">
+					<div class="single-team-member">
+						<div class="family-img">
+							<img class="img-fluid" src="images/fog.png" alt="" />
+						</div>
+						<div class="family-info">
+							<h4>Mr. Linus Njoku </h4>
+							<p>{ Grooms Father }</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6 col-sm-12">
+					<div class="single-team-member">
+						<div class="family-img">
+							<img class="img-fluid" src="images/mog.png" alt="" />
+						</div>
+						<div class="family-info">
+							<h4>Mrs. Bridget Njoku </h4>
+							<p>{ Grooms Mother }</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6 col-sm-12">
+					<div class="single-team-member">
+						<div class="family-img">
+							<img class="img-fluid" src="images/moh.png" alt="" />
+						</div>
+						<div class="family-info">
+							<h4>Ubong Lois Abia </h4>
+							<p>{ Maid Of Honor }</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6 col-sm-12">
+					<div class="single-team-member">
+						<div class="family-img">
+							<img class="img-fluid" src="images/bestman.png" alt="" />
+						</div>
+						<div class="family-info">
+							<h4>Jaja Ibifubara </h4>
+							<p>{ Best Man }</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End Family -->
+
+	<!-- Start Gallery -->
+	<div id="gallery" class="gallery-box">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="title-box">
+						<h2>Gallery</h2>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<ul class="popup-gallery clearfix">
+					<li>
+						<a href="images/1.png">
+							<img class="img-fluid" src="images/1.png" alt="single image">
+							<span class="overlay"><i class="fa fa-heart-o" aria-hidden="true"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="images/2.png">
+							<img class="img-fluid" src="images/2.png" alt="single image">
+							<span class="overlay"><i class="fa fa-heart-o" aria-hidden="true"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="images/7.png">
+							<img class="img-fluid" src="images/7.png" alt="single image">
+							<span class="overlay"><i class="fa fa-heart-o" aria-hidden="true"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="images/3.png">
+							<img class="img-fluid" src="images/3.png" alt="single image">
+							<span class="overlay"><i class="fa fa-heart-o" aria-hidden="true"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="images/8.png">
+							<img class="img-fluid" src="images/8.png" alt="single image">
+							<span class="overlay"><i class="fa fa-heart-o" aria-hidden="true"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="images/4.png">
+							<img class="img-fluid" src="images/4.png" alt="single image">
+							<span class="overlay"><i class="fa fa-heart-o" aria-hidden="true"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="images/5.png">
+							<img class="img-fluid" src="images/5.png" alt="single image">
+							<span class="overlay"><i class="fa fa-heart-o" aria-hidden="true"></i></span>
+						</a>
+					</li>
+					<li>
+						<a href="images/6.png">
+							<img class="img-fluid" src="images/6.png" alt="single image">
+							<span class="overlay"><i class="fa fa-heart-o" aria-hidden="true"></i></span>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<!-- End Gallery -->
+
+	<!-- Start Events -->
+	<div id="events" class="events-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="title-box">
+						<h2>Events</h2>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-4 col-md-6 col-sm-12">
+					<div class="event-inner">
+						<div class="event-img">
+							<img class="img-fluid" src="images/trad 1.png" alt="" />
+						</div>
+						<h2>10 December 2021 Traditional Wedding</h2>
+						<p>Papa O.S. Tedeye's Residents. Plot 75 Ighoyinvwinst. Off Etuwewe road by Warri City college Deco road Warri Delta State. <br><i class="fas fa-clock"></i> 02:00pm</p>
+						<a href="https://goo.gl/maps/9xu6CjMueDaU4utD8">See location <i class="fas fa-map-marker-alt"></i></a>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6 col-sm-12">
+					<div class="event-inner">
+						<div class="event-img">
+							<img class="img-fluid" src="images/Wedding-Ceremony.jpg" alt="" />
+						</div>
+						<h2>18 December 2021 Main Ceremony </h2>
+						<p>RCCG Resurrection Chapel, FCT 15 Headquaters Along Babangida Market, Lugbe Airpot Road Abuja. <br><i class="fas fa-clock"></i> 10:00am</p>
+						<a href="https://goo.gl/maps/Ucb6fgqW7grwMyQe7">See location <i class="fas fa-map-marker-alt"></i></a>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6 col-sm-12">
+					<div class="event-inner">
+						<div class="event-img">
+							<img class="img-fluid" src="images/recept 1.png" alt="" />
+						</div>
+						<h2>18 December 2021 Wedding party </h2>
+						<p>Tomaville Events Centre, Penthouse Estate 3 Along Pyakasa Road Lugbe Across, Abuja. <br><i class="fas fa-clock"></i> 01:00pm </p>
+						<a href="https://goo.gl/maps/7kzSPdFvjxLzj1Qw8">See location <i class="fas fa-map-marker-alt"></i> </a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End Events -->
+
+
+	<!-- Start Footer -->
+	<footer class="footer-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<p class="footer-company-name">All Rights Reserved. &copy; 2021 <a href="#">Our Wedding</a> Design By : <a href="#">Terrence Eze 08087908737</a></p>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!-- End Footer -->
+
+	<!-- ALL JS FILES -->
+	<script src="js/jquery.min.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<!-- ALL PLUGINS -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/jquery.pogo-slider.min.js"></script>
+	<script src="js/slider-index.js"></script>
+	<script src="js/smoothscroll.js"></script>
+	<script src="js/form-validator.min.js"></script>
+	<script src="js/contact-form-script.js"></script>
+	<script src="js/custom.js"></script>
+</body>
+
+</html>
